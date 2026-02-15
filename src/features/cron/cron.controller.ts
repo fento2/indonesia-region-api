1b8806549dd3ea88b9d23e9f350b5badc783da3a
fento2 <ftonrate91@gmail.com>
2026-02-16 03:30:46 +0700

    changes  vercel cron and, add random pingDb

--- src/features/cron/cron.controller.ts
+++ src/features/cron/cron.controller.ts
@@ -3,8 +3,16 @@ import { HttpStatus } from "../../constants/httpStatus";
 import AppError from "../../errors/appError";
 import { ExpressCbFn } from "../../types/shared";
 import { sendResponse } from "../../utils/sendResponse";
+import DistrictRepository from "../district/district.repository";
+import ProvinceRepository from "../province/province.repository";
+import RegencyRepository from "../regency/regency.repository";
+import VillageRepository from "../village/village.repository";
 
 class CronController {
+  private provinceRepository = new ProvinceRepository();
+  private regencyRepository = new RegencyRepository();
+  private villageReposiroty = new VillageRepository();
+  private districtRepository = new DistrictRepository();
   keepDbAlive: ExpressCbFn = async (req, res, next) => {
     try {
       const isVercelCron = req.headers["x-vercel-cron"];
@@ -12,8 +20,20 @@ class CronController {
       if (!isVercelCron) {
         throw new AppError("Unauthorized", HttpStatus.UNAUTHORIZED);
       }
+      const pingDbFunctions = [
+        () => this.provinceRepository.getProvinces({ include: [] }),
+        () =>
+          this.regencyRepository.getRegencies({
+            include: [],
+            type: "Kabupaten",
+          }),
+        () => this.villageReposiroty.getVillages({ include: [] }),
+        () => this.districtRepository.getDistricts({ include: [] }),
+      ];
 
-      await prisma.$queryRaw`SELECT 1`;
+      const randomIndex = Math.floor(Math.random() * pingDbFunctions.length);
+
+      await pingDbFunctions[randomIndex]();
 
       return sendResponse(
         res,
